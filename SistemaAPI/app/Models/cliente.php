7a6d3c248c03ffa0492816420a53b3d3d5d3a87d
Ballesd <ballesterosj500@gmail.com>
2022-07-14 18:59:21 -0500

    Revert "Nueva tabla muchos a muchos"
    This reverts commit cb90d03039ec62ca62a5c4fbe0698e9c3fa99be2.

--- SistemaAPI/app/Models/cliente.php
+++ SistemaAPI/app/Models/cliente.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class Cliente extends Model
+class cliente extends Model
 {
     use HasFactory;
 
     public function productos(){
 
-        return $this->belongsToMany(Producto::class, 'producto_clientes');
+        return $this->belongsToMany(producto::class, 'producto_clientes');
     }
 }
--- SistemaAPI/app/Models/producto.php
+++ SistemaAPI/app/Models/producto.php
@@ -5,15 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Producto extends Model
+class producto extends Model
 {
     use HasFactory;
-    /**
-     * The roles that belong to the producto
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
+
     public function clientes(){
-        return $this->belongsToMany(Cliente::class, 'producto_cliente');
+
+        return $this->belongsToMany(cliente::class, 'producto_clientes');
     }
 }
