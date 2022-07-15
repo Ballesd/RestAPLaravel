df5d5dc18f39f27eb7f43c08eb7b61ec077a22d8
Ballesd <ballesterosj500@gmail.com>
2022-07-15 09:41:05 -0500

    Rest API de la ralción N a N

--- SistemaAPI/app/Http/Controllers/RelacionACController.php
+++ SistemaAPI/app/Http/Controllers/RelacionACController.php
@@ -14,4 +14,12 @@ class RelacionACController extends Controller
         $materia = Materia::find(2);
         return view('welcome',compact('alumno','materia'));
     }
+
+    public function API()
+    {
+        $alumno = Alumno::find(1);
+        $materia = Materia::find(2);
+        return compact('alumno','materia');
+
+    }
 }
